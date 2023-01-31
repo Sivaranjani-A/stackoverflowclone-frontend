3ecfaffae56876eab0cac44fc4a73686b0a532a0
Sivaranjani-A <ranjani731997@gmail.com>
2023-01-31 16:46:53 +0530

    update

--- src/components/contents/QuestionCard.js
+++ src/components/contents/QuestionCard.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { config } from '../../config';
-import UserContext from '../../context/UserContext';
+
 
 function QuestionCard({ data }) {
     const [len, setlen] = useState();
